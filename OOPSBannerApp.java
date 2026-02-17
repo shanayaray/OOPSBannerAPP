1b7e2a0b8dd14ae6e4da3a7c2680ef160b8f2d68
Shanaya Ray <shanayaray449@gmail.com>
2026-02-17 13:50:27 +0530

    UC1

---
+++ OOPSBannerApp.java
@@ -0,0 +1,17 @@
+/**
+ * OOPSBannerApp - UC1 Implementation
+ * Prints the literal text "OOPS" to the console.
+ *
+ * @author Laksh
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Entry point of the application
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
